f1d7e6b867c862a020eb253d5e477d6ed5ce8d78
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-07-12 12:09:33 -0700

    feat:create main page layout

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,25 +1,57 @@
 export default function Blog() {
   return (
-    <div className="grid grid-cols-4 grid-rows-3">
-      <header className="border-2 col-span-4 h-12">header</header>
-      <aside className="border-2 h-24">aside</aside>
-      <article className="border-2 col-span-3 min-h-12">
-        <div>content1</div>
-        <div>content1</div>
-        <div>content1</div>
-        <div>content1</div>
-        <div>content1</div>
-        <div>content1</div>
-        <div>content1</div>
-        <div>content1</div>
-        <div>content1</div>
-        <div>content1</div>
-        <div>content1</div>
-        <div>content1</div>
-        <div>content1</div>
-        <div>content1</div>
-      </article>
-      <footer className="border-2 col-span-4 h-24">footer</footer>
+    <div className="flex flex-col relative">
+      <header className="fixed border-2 h-44 w-full">header</header>
+      <main className="flex w-full gap-8 mt-44">
+        <aside className="fixed border-2 w-1/6 h-[calc(100%-11rem)]">
+          aside
+        </aside>
+        <article className="border-2 w-5/6 ml-auto">
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+          <div>content1</div>
+        </article>
+      </main>
     </div>
   );
 }
