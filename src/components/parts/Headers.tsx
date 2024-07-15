76ea3f9634a8860bb3f3c37515abb0a7d4637538
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-07-15 10:59:30 -0700

    feat:search bar

--- src/components/parts/Headers.tsx
+++ src/components/parts/Headers.tsx
@@ -1,14 +1,25 @@
 export const Headers = () => {
   return (
     <header className="fixed border-2 h-24 w-full">
-      <div className="wrapper flex justify-between">
+      <div className="wrapper h-full flex justify-between items-center mx-4">
         <a href="/blog">
           <div>TITLE</div>
         </a>
         <div>
-          <form>
-            <input placeholder="search" />
-            <button>search</button>
+          <form className="flex gap-4">
+            <div className="bg-white flex border rounded-md p-2">
+              <img src="icons/search.svg" className="w-6  mr-2" />
+              <input
+                placeholder="search"
+                className="border-none focus:outline-none text-black"
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-white text-black rounded-md px-4"
+            >
+              search
+            </button>
           </form>
         </div>
         <div className="flex">
