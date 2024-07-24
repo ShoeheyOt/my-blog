f482a164343a1fce81836c67480e895806421ad5
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-07-23 23:33:59 -0700

    add card

--- src/components/parts/Articles.tsx
+++ src/components/parts/Articles.tsx
@@ -1,9 +1,24 @@
 import { ArticleCard } from "@/features/ArticleCard";
+import { faker } from "@faker-js/faker";
 
 export const Articles = () => {
+  const randomName = faker.person.fullName();
+  const randomTitle = faker.lorem.words({ min: 3, max: 10 });
+  const randomAvatar = faker.image.avatar();
+  const randomImage = faker.image.url();
+  const randomWord = faker.lorem.word();
+  const data = {
+    randomName,
+    randomTitle,
+    randomAvatar,
+    randomImage,
+    randomWord,
+  };
   return (
-    <article className="border-2 w-5/6 ml-auto">
-      <ArticleCard />
-    </article>
+    <div className="wrapper ml-auto border-2 w-5/6 bg-primary min-h-[calc(100vh-6rem)]">
+      <article className="flex flex-wrap h-full">
+        <ArticleCard data={data} />
+      </article>
+    </div>
   );
 };
--- src/features/ArticleCard.tsx
+++ src/features/ArticleCard.tsx
@@ -1,9 +1,35 @@
-export const ArticleCard = () => {
+interface ITempData {
+  randomName: string;
+  randomTitle: string;
+  randomAvatar: string;
+  randomImage: string;
+  randomWord: string;
+}
+export const ArticleCard = ({ data }: { data: ITempData }) => {
+  const { randomName, randomTitle, randomAvatar, randomImage, randomWord } =
+    data;
   return (
     <>
-      <div>
-        <div>article card</div>;
+      <div className="cardWrapper w-72 h-96 border-2 bg-warning rounded-lg">
+        <div className="h-2/5 w-full mb-2">
+          <img
+            src={randomImage}
+            className="h-full w-full object-cover rounded-t-lg"
+          />
+        </div>
+        <div className="text font-montserrat h-1/6 flex items-center gap-3 mx-2">
+          <img src={randomAvatar} className="w-8 h-8 rounded-full" />
+          <span className="name">{randomName}</span>
+        </div>
+        <div className="title mx-2">{randomTitle}</div>
       </div>
+      <div className="wrapper w-72 h-96 border-2 bg-warning">{randomName}</div>
+      <div className="wrapper w-72 h-96 border-2 bg-warning">{randomTitle}</div>
+      <div className="wrapper w-72 h-96 border-2 bg-warning"></div>
+      <div className="wrapper w-72 h-96 border-2 bg-warning"></div>
+      <div className="wrapper w-72 h-96 border-2 bg-warning">{randomWord}</div>
+      <div className="wrapper w-72 h-96 border-2 bg-warning">{randomName}</div>
+      <div className="wrapper w-72 h-96 border-2 bg-warning">{randomName}</div>
     </>
   );
 };
