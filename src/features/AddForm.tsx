58f384d3386f116a2a4701d1e54a677c4af869ee
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-07-31 11:13:57 -0700

    modify background color

--- src/features/AddForm.tsx
+++ src/features/AddForm.tsx
@@ -29,44 +29,46 @@ export const AddForm = () => {
   };
 
   return (
-    <div className="h-full mx-96">
-      <header className="postBlogWrapper border-2 rounded-lg flex justify-between items-center my-8 px-12 bg-secondary text-primary">
-        <div className="text-center my-8 text-D_title font-montserrat font-extralight">
-          New Post
-        </div>
-        <Button onClick={handleBack} className="bg-danger">
-          Back
-        </Button>
-      </header>
-      <form
-        onSubmit={handleSubmit}
-        className="w-full border-2 mx-auto my-auto flex justify-center rounded-lg bg-secondary"
-      >
-        <div className="addPostWrapper flex flex-col font-montserrat gap-2 w-full px-12 py-8 h-[calc(100dvh/1.5)]">
-          <label className="text-primary text-D_text font-extralight">
-            Title:
-            <input
-              ref={titleRef}
-              name="title"
-              className="border-2 rounded-lg w-full pl-2 py-1 font-extralight text-D_notes bg-primary text-secondary focus:outline-none"
-            />
-          </label>
-          <label className="w-full h-full font-extralight text-primary text-D_text">
-            Text:
-            <textarea
-              ref={textRef}
-              name="text"
-              className="border-2 rounded-lg w-full h-[calc(100%-3rem)] pl-2 pt-1 font-extralight text-D_notes bg-primary focus:outline-none text-secondary"
-            />
-          </label>
-          <div className="submitWrapper flex justify-between">
-            <Button onClick={(e) => handleCancel(e)}>cancel</Button>
-            <Button buttonType="submit" className="bg-info">
-              Submit
-            </Button>
+    <div className="w-screen h-screen bg-light py-8">
+      <div className="h-full mx-96">
+        <header className="postBlogWrapper border-2 rounded-lg flex justify-between items-center px-12 bg-secondary text-primary">
+          <div className="text-center my-8 text-D_title font-montserrat font-extralight">
+            New Post
           </div>
-        </div>
-      </form>
+          <Button onClick={handleBack} className="bg-danger">
+            Back
+          </Button>
+        </header>
+        <form
+          onSubmit={handleSubmit}
+          className="w-full border-2 mx-auto my-auto flex justify-center rounded-lg bg-secondary"
+        >
+          <div className="addPostWrapper flex flex-col font-montserrat gap-2 w-full px-12 py-8 h-[calc(100dvh/1.5)]">
+            <label className="text-primary text-D_text font-extralight">
+              Title:
+              <input
+                ref={titleRef}
+                name="title"
+                className="border-2 rounded-lg w-full pl-2 py-1 font-extralight text-D_notes bg-primary text-secondary focus:outline-none"
+              />
+            </label>
+            <label className="w-full h-full font-extralight text-primary text-D_text">
+              Text:
+              <textarea
+                ref={textRef}
+                name="text"
+                className="border-2 rounded-lg w-full h-[calc(100%-3rem)] pl-2 pt-1 font-extralight text-D_notes bg-primary focus:outline-none text-secondary"
+              />
+            </label>
+            <div className="submitWrapper flex justify-between">
+              <Button onClick={(e) => handleCancel(e)}>cancel</Button>
+              <Button buttonType="submit" className="bg-info">
+                Submit
+              </Button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
