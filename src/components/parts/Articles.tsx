582bc44996b7fa3953b89df84399731abb6ded02
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-08-22 08:20:48 -0700

    update design

--- src/components/parts/Articles.tsx
+++ src/components/parts/Articles.tsx
@@ -2,6 +2,7 @@ import { ArticleCard } from "@/features/ArticleCard";
 import { articleFakeData } from "../../../public/articleFakeData";
 import {
   Card,
+  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
@@ -14,23 +15,11 @@ export const Articles = () => {
   const fakeData = articleFakeData;
   return (
     <div className="wrapper ml-auto border-2 w-5/6 bg-light min-h-[calc(100vh-6rem)]">
-      <article className="flex flex-wrap h-full">
+      <article>
         {fakeData.map((data, i) => (
           <ArticleCard key={i} data={data} />
         ))}
       </article>
-      <Link href="blog/add">
-        <Card className="border-2 bg-dark m-10">
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>this is description</CardDescription>
-          </CardHeader>
-          <CardFooter className="flex justify-end gap-4">
-            <Button className="bg-danger text-secondary">Delete</Button>
-            <Button>Edit</Button>
-          </CardFooter>
-        </Card>
-      </Link>
     </div>
   );
 };
--- src/features/ArticleCard.tsx
+++ src/features/ArticleCard.tsx
@@ -1,43 +1,35 @@
 "use client";
 import Link from "next/link";
 import { IArticleFake } from "../../public/articleFakeData";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 export const ArticleCard = ({ data }: { data: IArticleFake }) => {
   const { id, title, tags, imageUrl, author, createdAt } = data;
 
   return (
-    <Link
-      href={`/blog/${id}`}
-      className="hover:transition-all hover:scale-95 hover:opacity-50 cursor-pointer"
-    >
-      <div className="cardWrapper w-96 h-[28rem] border-2 bg-warning rounded-lg">
-        <div className="h-2/5 w-full mb-2">
-          <img
-            src={imageUrl}
-            className="h-full w-full object-cover rounded-t-lg"
-          />
-        </div>
-        <div className="articleWrapper h-3/5 mx-2 py-4 flex flex-col justify-between">
-          <div className="mainCardWrapper flex flex-col gap-3">
-            <div className="date text-D_notes font-extralight">{createdAt}</div>
-            <div className="text h-1/6 flex items-center gap-3">
-              <img src={author.authorAvatar} className="w-8 h-8 rounded-full" />
-              <span className="name">{author.authorName}</span>
-            </div>
-            <div className="title text-D_text">{title}</div>
-          </div>
-          <div className="tagWrapper">
-            {tags.map((tag, i) => (
-              <span
-                key={i}
-                className="tag text-D_notes text-opacity-80 px-2 py-1 mr-1 bg-secondary text-primary rounded-full"
-              >
-                #{tag}
-              </span>
-            ))}
-          </div>
-        </div>
-      </div>
-    </Link>
+    <Card className="border-2 bg-dark m-10">
+      <Link href={`/blog/${id}`}>
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{createdAt}</CardDescription>
+        </CardHeader>
+      </Link>
+      <CardFooter className="flex justify-end gap-4">
+        <Button
+          onClick={() => console.log("click delete")}
+          className="bg-danger text-secondary"
+        >
+          Delete
+        </Button>
+        <Button onClick={() => console.log("clicked edit")}>Edit</Button>
+      </CardFooter>
+    </Card>
   );
 };
