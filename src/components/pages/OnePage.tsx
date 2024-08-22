e1f1159cd6d7be3de1cbd5d698a9bb9c49d29daa
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-08-22 11:15:28 -0700

    add edit delete button

--- src/components/pages/OnePage.tsx
+++ src/components/pages/OnePage.tsx
@@ -9,30 +9,26 @@ export const OnePage = ({
   articleId: number;
   articleFakeOneData: IArticleFakeOneData;
 }) => {
-  const { title, text, tags, createdAt, imageUrl, author } = articleFakeOneData;
+  const { title, text, tags, createdAt, author } = articleFakeOneData;
   return (
     <div className="oneArticleWrapper px-96 min-h-screen bg-primary">
       <div className="text-D_title font-thin">{title}</div>
-      <div className="text-end">{author.authorName}</div>
+      <div className="flex gap-4">
+        <div className="text-end">{author.authorName} | </div>
+        <div className="self-end">{createdAt}</div>
+      </div>
       <div className="my-10 text-D_text min-h-80 font-thin text-center">
         {text}
       </div>
 
-      <div className="tagsWrapper">
-        {tags.map((tag, i) => (
-          <span
-            key={i}
-            className="text-D_notes border-2 border-secondary px-2 rounded-full mr-1"
-          >
-            # {tag}
-          </span>
-        ))}
-      </div>
-      <div className="flex flex-col gap-4 mt-4">
-        <div className="self-end">{createdAt}</div>
-        <Button asChild className="self-end">
+      <div className="flex mt-4 justify-between">
+        <Button asChild className="">
           <Link href="/blog">Back</Link>
         </Button>
+        <div className="flex gap-2">
+          <Button>Edit</Button>
+          <Button variant="destructive">Delete</Button>
+        </div>
       </div>
     </div>
   );
